f3631c994ec3bfe16f517f61eba05739000d4fca
Ncebakazi <ncebakazi414@gmail.com>
2021-06-09 19:17:57 +0200

    format

--- registration_template/registrationNum_template.js
+++ registration_template/registrationNum_template.js
@@ -12,184 +12,171 @@ var templateSource = document.querySelector(".userTemplate").innerHTML;
 var userTemplate = Handlebars.compile(templateSource);
 
 
-//old code
-//var RegistrationNum = registration_numbers()
-
 function textclear_temp() {
-    textareaElem.value = ""
-    document.getElementById('Cape_temp').checked = false
-    document.getElementById('Bellville_temp').checked = false
-    document.getElementById('Paarl_temp').checked = false
-  
+  textareaElem.value = ""
+  document.getElementById('Cape_temp').checked = false
+  document.getElementById('Bellville_temp').checked = false
+  document.getElementById('Paarl_temp').checked = false
+
+}
+var registrationList_temp = JSON.parse(localStorage.getItem('registrations_temp'))
+var RegistrationNum = registration_numbers(registrationList_temp)
+var regsTemplate = []
+
+if (registrationList_temp) {
+  for (var i = 0; i < registrationList_temp.length; i++) {
+    var x = registrationList_temp[i];
+    var obj = { regNumber: x };
+    regsTemplate.push(obj)
   }
-  var registrationList_temp = JSON.parse(localStorage.getItem('registrations_temp'))
-  var RegistrationNum = registration_numbers(registrationList_temp)
+}
+var userDataHTML = userTemplate({
+
+  RegList: regsTemplate
+})
+
+list_tempElem.innerHTML = userDataHTML
+function registration_temp() {
+  var value1 = document.querySelector("#form_temp").value
+  var arrayReg = regsTemplate
+
+
+  if (value1 === "") {
+    setTimeout(function () {
+      displayMessageElem.innerHTML = "Please enter registration number"
+      displayMessageElem.classList.add("error")
+    }, 0);
+    setTimeout(function () {
+      displayMessageElem.innerHTML = ""
+    }, 2000);
+    return false
+  }
+  RegistrationNum.setReg(value1)
+  registrationList_temp = JSON.parse(localStorage.getItem('registrations_temp'))
+  console.log(RegistrationNum.setReg(value1))
+  if (RegistrationNum.setReg(value1)) {
+
+    var obj = { regNumber: value1 };
+    arrayReg.push(obj)
+
+    var userDataHTML = userTemplate({
+      RegList: arrayReg
+    })
+
+    list_tempElem.innerHTML = userDataHTML
+
+    if (registrationList_temp) {
+      if (RegistrationNum.isReapted(registrationList_temp)) {
+        setTimeout(function () {
+          displayMessageElem.innerHTML = "This registration number already exist"
+          displayMessageElem.classList.add("error")
+        }, 0);
+        setTimeout(function () {
+          displayMessageElem.innerHTML = ""
+        }, 2000);
+
+      } else {
+        localStorage.setItem('registrations_temp', JSON.stringify(RegistrationNum.getReglist()));
+      }
+    } else {
+      localStorage.setItem('registrations_temp', JSON.stringify(RegistrationNum.getReglist()));
+    }
+    textclear_temp()
+  }
+  else {
+    setTimeout(function () {
+      displayMessageElem.innerHTML = "Please follow this format CA 123-123/ CA 123 123"
+      displayMessageElem.classList.add("error")
+
+    }, 0);
+    setTimeout(function () {
+      displayMessageElem.innerHTML = ""
+    }, 2000);
+  }
+}
+
+function forEachTown_temp() {
+  var arrayReg = []
+  var radiobutton = document.querySelector(".reg_temp:checked")
+  document.querySelector(".myList_temp").innerHTML = ""
+  if (radiobutton) {
+    var townList = RegistrationNum.forTown(radiobutton.value)
+
+    if (townList.length !== 0) {
+      for (var i = 0; i < townList.length; i++) {
+        if (townList[i].startsWith(radiobutton.value)) {
+          var obj = { regNumber: townList[i] };
+          arrayReg.push(obj)
+
+          displayMessageElem.innerHTML = ""
+          textclear_temp()
+        }
+      }
+    }
+    else {
+      setTimeout(function () {
+        displayMessageElem.innerHTML = "There is no registration number for this town"
+        displayMessageElem.classList.add("error")
+      }, 0);
+      setTimeout(function () {
+        displayMessageElem.innerHTML = ""
+        textclear_temp()
+      }, 2000);
+    }
+  }
+  else {
+    setTimeout(function () {
+      displayMessageElem.innerHTML = "Please select a town"
+      displayMessageElem.classList.add("error")
+    }, 0);
+    setTimeout(function () {
+      displayMessageElem.innerHTML = ""
+    }, 2000);
+    textclear_temp()
+  }
+  var userDataHTML = userTemplate({
+    RegList: arrayReg
+  })
+
+  list_tempElem.innerHTML = userDataHTML
+
+}
+function displayAll_temp() {
   var regsTemplate = []
+  document.querySelector(".myList_temp").innerHTML = ""
 
   if (registrationList_temp) {
     for (var i = 0; i < registrationList_temp.length; i++) {
       var x = registrationList_temp[i];
-      var obj = {regNumber: x};
+      var obj = { regNumber: x };
       regsTemplate.push(obj)
     }
-  }
-  var userDataHTML = userTemplate({
-
-    RegList: regsTemplate
-  })
-
-  list_tempElem.innerHTML = userDataHTML
-  function registration_temp() {
-    var value1 = document.querySelector("#form_temp").value
-    var arrayReg = regsTemplate
-   
- 
-    if (value1 === "") {
-      setTimeout(function () {
-        displayMessageElem.innerHTML = "Please enter registration number"
-        displayMessageElem.classList.add("error")
-      }, 0);
-      setTimeout(function () {
-        displayMessageElem.innerHTML = ""
-      }, 2000);
-      return false
-    }
-    RegistrationNum.setReg(value1)
-    registrationList_temp = JSON.parse(localStorage.getItem('registrations_temp'))
-    console.log(RegistrationNum.setReg(value1))
-    if(RegistrationNum.setReg(value1)) {
-
-      var obj = {regNumber: value1};
-      arrayReg.push(obj)
-  
-      var userDataHTML = userTemplate({
-         RegList: arrayReg
-      })
-    
-      list_tempElem.innerHTML = userDataHTML
-   
-      if (registrationList_temp) {
-        if (RegistrationNum.isReapted(registrationList_temp)) {
-          setTimeout(function () {
-            displayMessageElem.innerHTML = "This registration number already exist"
-            displayMessageElem.classList.add("error")
-          }, 0);
-          setTimeout(function () {
-            displayMessageElem.innerHTML = ""
-          }, 2000);
-  
-        } else {
-          localStorage.setItem('registrations_temp', JSON.stringify(RegistrationNum.getReglist()));
-          //appendElement2(value1)
-        }
-      } else {
-        localStorage.setItem('registrations_temp', JSON.stringify(RegistrationNum.getReglist()));
-       // appendElement2(value1)
-      }
-      textclear_temp()
-    }
-    else{
-        setTimeout(function(){
-        displayMessageElem.innerHTML = "Please follow this format CA 123-123/ CA 123 123"
-        displayMessageElem.classList.add("error")
-       
-      },0);
-      setTimeout(function(){
-        displayMessageElem.innerHTML = ""
-      }, 2000); 
-    }
-  }
-  
-  function forEachTown_temp() {
-    var arrayReg = []
-    var radiobutton = document.querySelector(".reg_temp:checked")
-    document.querySelector(".myList_temp").innerHTML = ""
-    if(radiobutton){
-      var townList= RegistrationNum.forTown(radiobutton.value)
-  
-      if(townList.length !== 0){
-        for (var i = 0; i < townList.length; i++){
-          if (townList[i].startsWith(radiobutton.value)) {
-            var obj = {regNumber: townList[i]};
-            arrayReg.push(obj)
-            //appendElement2(townList[i])
-          
-            displayMessageElem.innerHTML = ""
-            textclear_temp()
-          }
-        }
-      }
-        else {
-          
-          setTimeout(function () {
-            displayMessageElem.innerHTML = "There is no registration number for this town"
-            displayMessageElem.classList.add("error")
-          }, 0);
-          setTimeout(function () {
-            displayMessageElem.innerHTML = ""
-            textclear_temp()
-          }, 2000);
-      }
-    } 
-    else {
-      setTimeout(function () {
-        displayMessageElem.innerHTML = "Please select a town"
-        displayMessageElem.classList.add("error")
-      }, 0);
-      setTimeout(function () {
-        displayMessageElem.innerHTML = ""
-      }, 2000);
-      textclear_temp()
-    }
     var userDataHTML = userTemplate({
-      RegList: arrayReg
-   })
- 
-   list_tempElem.innerHTML = userDataHTML
-    
+
+      RegList: regsTemplate
+    })
+
+    list_tempElem.innerHTML = userDataHTML.classList.add('reg_number_temp');
   }
-  function displayAll_temp() {
-    var regsTemplate = []
-     document.querySelector(".myList_temp").innerHTML = ""
-   
-     if (registrationList_temp) {
-      for (var i = 0; i < registrationList_temp.length; i++) {
-        var x = registrationList_temp[i];
-        var obj = {regNumber: x};
-        regsTemplate.push(obj)
-      }
-      var userDataHTML = userTemplate({
-  
-        RegList: regsTemplate
-      })
-    
-      list_tempElem.innerHTML = userDataHTML.classList.add('reg_number_temp');
-    }
-    
-    else {
-      setTimeout(function () {
-        displayMessageElem.innerHTML = "Currently there are no existing registration numbers"
-        displayMessageElem.classList.add("error")
-      }, 0);
-      setTimeout(function () {
-        displayMessageElem.innerHTML = ""
-  
-      }, 2000);
-    }
-  //   var userDataHTML = userTemplate({
-  //     RegList: arrayReg
-  //  })
- 
-  //  list_tempElem.innerHTML = userDataHTML
-   location.reload() 
+
+  else {
+    setTimeout(function () {
+      displayMessageElem.innerHTML = "Currently there are no existing registration numbers"
+      displayMessageElem.classList.add("error")
+    }, 0);
+    setTimeout(function () {
+      displayMessageElem.innerHTML = ""
+
+    }, 2000);
   }
-  
-  
-  addbtn_temp.addEventListener('click', registration_temp)
-  townbtn_temp.addEventListener('click', forEachTown_temp)
-  displaybtn_temp.addEventListener('click', displayAll_temp)
-  
-  clearbtn_temp.addEventListener('click', function () {
-    localStorage.removeItem("registrations_temp")
-    location.reload()
-  });
+  location.reload()
+}
+
+addbtn_temp.addEventListener('click', registration_temp)
+townbtn_temp.addEventListener('click', forEachTown_temp)
+displaybtn_temp.addEventListener('click', displayAll_temp)
+
+clearbtn_temp.addEventListener('click', function () {
+  localStorage.removeItem("registrations_temp")
+  location.reload()
+});
